e767af195663c2bff1c948e6be69f8009f889442
Shayan Shojaei <shayanshojaei.2002@gmail.com>
2021-05-11 23:19:40 +0430

    Implement Instagram Process

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { program } = require("commander");
 const { setup, askCredentials } = require("./lib/inquirer");
+const { updateInstagram } = require("./lib/instagram_util");
 
 program.version(
   require("./package").version,
@@ -10,25 +11,24 @@ program.version(
 program.description("set a profile picture for all your social media accounts");
 program.parse();
 
-let selectedPlatforms = [];
-let inputPath = "";
-let inputCredentials = {};
-setup()
-  .then(({ platforms, path }) => {
-    selectedPlatforms = platforms;
-    inputPath = path;
-    return askCredentials(platforms);
-  })
-  .then((credentials) => {
-    selectedPlatforms.forEach((platform) => {
+(async () => {
+  try {
+    let inputCredentials = {};
+    const { platforms, path } = await setup();
+    const credentials = await askCredentials(platforms);
+    platforms.forEach((platform) => {
       inputCredentials[platform.toLocaleLowerCase()] = {
         username: credentials[`${platform}_username`.toLocaleLowerCase()],
         password: credentials[`${platform}_password`.toLocaleLowerCase()],
       };
     });
-    console.log(inputCredentials);
-  })
-  .catch((err) => {
+    if (Object.keys(inputCredentials).includes("instagram")) {
+      await updateInstagram(inputCredentials["instagram"], path);
+    }
+  } catch (err) {
     console.error("Unexpected error!");
     console.error(err);
-  });
+  } finally {
+    process.exit(0);
+  }
+})();
